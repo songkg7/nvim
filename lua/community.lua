28457573625aa46ebe289ca488420b617003959c
haril song <songkg7@gmail.com>
2025-04-02 00:05:05 +0900

    add copilot

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +8,5 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
